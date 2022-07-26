0e846717c66667973822d9efaff981831c87f750
OlgaSivyuk <olchik0308@yandex.ru>
2022-07-26 18:18:50 +0400

    feat% backend infrastructure

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+// const bodyParser = require('body-parser');
+// const cookieParser = require('cookie-parser');
+
+const { PORT = 3000 } = process.env;
+const app = express();
+
+// подключаемся к серверу mongo
+mongoose.connect('mongodb://localhost:27017/moviesdb');
+
+app.listen(PORT, () => {
+  console.log('App started and listen port', PORT);
+});
