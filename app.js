24cd2f7a053bd6266994d4e5628339dff2c201df
OlgaSivyuk <olchik0308@yandex.ru>
2022-07-27 15:04:20 +0400

    feat: add main folders

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const { PORT = 3000 } = process.env;
 const app = express();
 
 // подключаемся к серверу mongo
-mongoose.connect('mongodb://localhost:27017/moviesdb');
+mongoose.connect('mongodb://localhost:27017/bitfilmsdb');
 
 app.listen(PORT, () => {
   console.log('App started and listen port', PORT);
